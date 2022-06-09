ee7e4e9930464fc50a17305128c191de29e36276
Albert <47365826+os3albert@users.noreply.github.com>
2022-06-09 17:47:45 +0200

    first commit

---
+++ src/test/java/com/acme/sanzioni/ProgettoSanzioniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.sanzioni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoSanzioniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
