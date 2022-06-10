0195ab8f20329a8b1c730fb81d83ea1087ca4142
Albert <47365826+os3albert@users.noreply.github.com>
2022-06-10 16:46:39 +0200

    Builded personaFactory.java

--- src/main/java/com/acme/sanzioni/persona/PersonaFactory.java
+++ src/main/java/com/acme/sanzioni/persona/PersonaFactory.java
@@ -5,8 +5,16 @@ public class PersonaFactory {
 	public static final int CRIMINALE = 0;
 	public static final int POLIZIOTTO = 1;
 	
-	public static Persona create (int tipo) {
-		Persona persona = new 
+	public static Persona create (int tipo, String nome, String cognome) {
+		Persona persona = null;
+		
+		if (tipo == 0) {
+			persona = new Criminale(nome, cognome);
+		}else {
+			persona = new Poliziotto(nome, cognome);
+		}
+		
+		return persona;
 	}
 
 }
