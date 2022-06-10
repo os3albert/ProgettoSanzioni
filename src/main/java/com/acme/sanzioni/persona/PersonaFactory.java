8cb128f77402fb1ce0d38a31286586be3b1cc64b
Albert <47365826+os3albert@users.noreply.github.com>
2022-06-10 16:52:35 +0200

    Added Exception for PersonaFactory

--- src/main/java/com/acme/sanzioni/persona/PersonaFactory.java
+++ src/main/java/com/acme/sanzioni/persona/PersonaFactory.java
@@ -5,16 +5,19 @@ public class PersonaFactory {
 	public static final int CRIMINALE = 0;
 	public static final int POLIZIOTTO = 1;
 	
-	public static Persona create (int tipo, String nome, String cognome) {
+	public static Persona create (int tipo, String nome, String cognome) throws WrongPersonUsedException {
+		
 		Persona persona = null;
 		
 		if (tipo == 0) {
 			persona = new Criminale(nome, cognome);
-		}else {
+		}else if(tipo == 1){
 			persona = new Poliziotto(nome, cognome);
+		}else {
+			throw new WrongPersonUsedException("la persona creata non esiste");
 		}
 		
 		return persona;
 	}
-
+	
 }
---
+++ src/main/java/com/acme/sanzioni/persona/WrongPersonUsedException.java
@@ -0,0 +1,30 @@
+package com.acme.sanzioni.persona;
+
+public class WrongPersonUsedException extends Exception {
+
+	public WrongPersonUsedException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public WrongPersonUsedException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public WrongPersonUsedException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public WrongPersonUsedException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public WrongPersonUsedException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
