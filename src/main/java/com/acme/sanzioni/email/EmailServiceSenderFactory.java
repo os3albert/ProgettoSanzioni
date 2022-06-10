5afefd8e8a2018134d24c30876cfc21902684059
Albert <47365826+os3albert@users.noreply.github.com>
2022-06-10 16:58:46 +0200

    Updated EmailServiceSenderFactory using EmailService interface

--- src/main/java/com/acme/sanzioni/email/EmailServiceSenderFactory.java
+++ src/main/java/com/acme/sanzioni/email/EmailServiceSenderFactory.java
@@ -5,7 +5,7 @@ public class EmailServiceSenderFactory {
 	public static final String UFFICIO_PROVINCIALE = "server.ufficio.provinciale.it";
 	public static final String UFFICIO_POLIZIA = "server.ufficio.polizia.it";
 
-	public static EmailServiceSender create(String tipo, String usernamePerInvio, String password) {
+	public static EmailService create(String tipo, String usernamePerInvio, String password) {
 		
 		EmailServiceSender emailServiceSender = null;
 		
